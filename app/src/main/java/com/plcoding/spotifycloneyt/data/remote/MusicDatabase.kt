c73f8afc7931fd96f3ffeeaf1867184480ba6243
Omar Adel <omarzero1015@gmail.com>
2021-01-06 19:22:03 +0200

    add firebase

---
+++ app/src/main/java/com/plcoding/spotifycloneyt/data/remote/MusicDatabase.kt
@@ -0,0 +1,21 @@
+package com.plcoding.spotifycloneyt.data.remote
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.plcoding.spotifycloneyt.data.entities.Song
+import com.plcoding.spotifycloneyt.data.utils.Constants.SONGS_COLLECTION
+import kotlinx.coroutines.tasks.await
+
+class MusicDatabase {
+
+    private val fireStore = FirebaseFirestore.getInstance()
+    private val songCollection = fireStore.collection(SONGS_COLLECTION)
+
+    suspend fun getAllSongs(): List<Song> {
+        return try {
+            songCollection.get().await().toObjects(Song::class.java)
+        } catch (e: Exception) {
+            emptyList()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/plcoding/spotifycloneyt/data/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.plcoding.spotifycloneyt.data.utils
+
+object Constants {
+    const val SONGS_COLLECTION = "songs"
+}
